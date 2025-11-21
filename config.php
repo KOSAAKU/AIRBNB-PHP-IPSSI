d45343ecb72099cb7aca7cdd419d748a5f93c47f
KOSAAKU <kosaaku@gmail.com>
2025-11-21 10:46:41 +0100

    update config.php

--- config.php
+++ config.php
@@ -1,15 +1,12 @@
 <?php
 
-
-// Remplace 'root' et '' par tes identifiants (souvent 'root', 'root' sur MAMP ou 'root', '' sur WAMP/XAMPP)
 $host = 'localhost';
-$dbname = 'airbnb';
+$dbname = 'Airbnb'; 
 $user = 'root';
 $pass = ''; 
 
 try {
     $dbh = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
-
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
